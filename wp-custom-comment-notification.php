81338856b0a17d100530771653a648b27b13df54
Dan Grebb <dgrebb@gmail.com>
2013-08-05 16:17:56 -0400

    updated comment notification plugin

--- wp-custom-comment-notification.php
+++ wp-custom-comment-notification.php
@@ -62,16 +62,17 @@ function wp_notify_postauthor( $comment_id, $comment_type = '' ) {
     if ( empty( $comment_type ) ) $comment_type = 'comment';
 
     if ('comment' == $comment_type) {
-        $notify_message  = sprintf( __( 'New comment on your post "%s"' ), $post->post_title ) . "\r\n";
+
+        $notify_message  = sprintf( __( 'New comment on your post "%s"' ), $post->post_title ) . "\r\n\r\n";
         /* translators: 1: comment author, 2: author IP, 3: author domain */
-        $notify_message .= sprintf( __('Author : %1$s'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
-        $notify_message .= sprintf( __('E-mail : %s'), $comment->comment_author_email ) . "\r\n";
-        $notify_message .= sprintf( __('URL    : %s'), $comment->comment_author_url ) . "\r\n";
+        $notify_message .= sprintf( __('Author: %1$s'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n\r\n";
         $notify_message .= __('Comment: ') . "\r\n" . $comment->comment_content . "\r\n\r\n";
-        $notify_message .= __('You can see all comments on this post here: ') . "\r\n";
+        $notify_message .= __('You can see all comments on this post here: ') . "\r\n\r\n";
         /* translators: 1: blog name, 2: post title */
         $subject = sprintf( __('[%1$s] Comment: "%2$s"'), $blogname, $post->post_title );
+
     } elseif ('trackback' == $comment_type) {
+
         $notify_message  = sprintf( __( 'New trackback on your post "%s"' ), $post->post_title ) . "\r\n";
         /* translators: 1: website name, 2: author IP, 3: author domain */
         $notify_message .= sprintf( __('Website: %1$s (IP: %2$s , %3$s)'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
@@ -80,7 +81,9 @@ function wp_notify_postauthor( $comment_id, $comment_type = '' ) {
         $notify_message .= __('You can see all trackbacks on this post here: ') . "\r\n";
         /* translators: 1: blog name, 2: post title */
         $subject = sprintf( __('[%1$s] Trackback: "%2$s"'), $blogname, $post->post_title );
+
     } elseif ('pingback' == $comment_type) {
+
         $notify_message  = sprintf( __( 'New pingback on your post "%s"' ), $post->post_title ) . "\r\n";
         /* translators: 1: comment author, 2: author IP, 3: author domain */
         $notify_message .= sprintf( __('Website: %1$s (IP: %2$s , %3$s)'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
@@ -89,14 +92,15 @@ function wp_notify_postauthor( $comment_id, $comment_type = '' ) {
         $notify_message .= __('You can see all pingbacks on this post here: ') . "\r\n";
         /* translators: 1: blog name, 2: post title */
         $subject = sprintf( __('[%1$s] Pingback: "%2$s"'), $blogname, $post->post_title );
+
     }
     $notify_message .= get_permalink($comment->comment_post_ID) . "#comments\r\n\r\n";
-    $notify_message .= sprintf( __('Permalink: %s'), get_permalink( $comment->comment_post_ID ) . '#comment-' . $comment_id ) . "\r\n";
-    if ( EMPTY_TRASH_DAYS )
-        $notify_message .= sprintf( __('Trash it: %s'), admin_url("comment.php?action=trash&c=$comment_id") ) . "\r\n";
-    else
-        $notify_message .= sprintf( __('Delete it: %s'), admin_url("comment.php?action=delete&c=$comment_id") ) . "\r\n";
-    $notify_message .= sprintf( __('Spam it: %s'), admin_url("comment.php?action=spam&c=$comment_id") ) . "\r\n";
+    // $notify_message .= sprintf( __('Permalink: %s'), get_permalink( $comment->comment_post_ID ) . '#comment-' . $comment_id ) . "\r\n";
+    // if ( EMPTY_TRASH_DAYS )
+    //     $notify_message .= sprintf( __('Trash it: %s'), admin_url("comment.php?action=trash&c=$comment_id") ) . "\r\n";
+    // else
+    //     $notify_message .= sprintf( __('Delete it: %s'), admin_url("comment.php?action=delete&c=$comment_id") ) . "\r\n";
+    // $notify_message .= sprintf( __('Spam it: %s'), admin_url("comment.php?action=spam&c=$comment_id") ) . "\r\n";
 
     $wp_email = 'wordpress@' . preg_replace('#^www\.#', '', strtolower($_SERVER['SERVER_NAME']));
 
